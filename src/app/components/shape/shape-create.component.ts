c31f5a121bd2bb8e064795bbd308130f21fdc9ee
Luis Gabriel Mena Arnedo <lgm3na@gmail.com>
2023-06-19 22:35:10 +1000

    Added logic to generate the shapes

--- src/app/components/shape/shape-create.component.ts
+++ src/app/components/shape/shape-create.component.ts
@@ -19,6 +19,7 @@ export class ShapeCreateComponent implements OnInit {
   shapeList = [
     { key: "", value: "" },
     { key: "circle", value: "Circle" },
+    { key: "equilateral_triangle", value: "Equilateral Triangle" },
     { key: "hexagon", value: "Hexagon" },
     { key: "heptagon", value: "Heptagon" },
     { key: "isosceles_triangle", value: "Isosceles Triangle" },
@@ -29,12 +30,12 @@ export class ShapeCreateComponent implements OnInit {
     { key: "rectangle", value: "Rectangle" },
     { key: "square", value: "Square" },
     { key: "scalene_triangle", value: "Scalene Triangle" },
-    { key: "triangle", value: "Triangle" },
   ];
 
   form = this.fb.group({
     shape: ["", { validators: [Validators.required] }],
     radius: ["", { validators: [Validators.pattern(/[0-9]/)] }],
+    radius2: ["", { validators: [Validators.pattern(/[0-9]/)] }],
     side: ["", { validators: [Validators.pattern(/[0-9]/)] }],
     side2: ["", { validators: [Validators.pattern(/[0-9]/)] }],
     side3: ["", { validators: [Validators.pattern(/[0-9]/)] }],
@@ -49,20 +50,108 @@ export class ShapeCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.canvasContext = this.canvas.nativeElement.getContext('2d');
+    //this.drawPolygon(this.canvasContext, 5, 50, 0, 5.026548245743669, 50, 50, 50);
   }
 
   get shape() { return this.form.controls["shape"]; }
 
   async onSubmit() {
+    console.log(this.shapeToRender);
     const formData = this.form.value;
     const canvasWidth = this.canvas.nativeElement.width;
     const canvasHeight = this.canvas.nativeElement.height;
-    this.shapeSvc.getShape(formData.radius, canvasWidth, canvasHeight)
-      .subscribe(response => {
+    switch (this.shapeToRender) {
+      case "circle":
 
-        this.drawCircle(this.canvasContext, this.canvas, formData.radius, response.x, response.y);
+        this.shapeSvc.getCicle(formData.shape, formData.radius, canvasWidth, canvasHeight)
+          .subscribe(response => {
 
-      })
+            this.drawCircle(this.canvasContext, this.canvas, formData.radius, response.x, response.y);
+
+          });
+        break;
+      case "oval":
+
+        this.shapeSvc.getOval(formData.shape, formData.radius, formData.radius2, canvasWidth, canvasHeight)
+          .subscribe(response => {
+
+            this.drawOval(this.canvasContext, this.canvas, formData.radius, formData.radius2, response.x, response.y);
+
+          });
+        break;
+      case "pentagon":
+        this.shapeSvc.getComplexShape(formData.shape, formData.side)
+          .subscribe(response => {
+            console.log([response.side, response.radius]);
+            this.drawPolygon(this.canvasContext, response.side, response.radius, response.x, response.y);
+
+          });
+        break;
+      case "hexagon":
+        this.shapeSvc.getComplexShape(formData.shape, formData.side)
+          .subscribe(response => {
+            console.log([response.side, response.radius]);
+            this.drawPolygon(this.canvasContext, response.side, response.radius, response.x, response.y);
+
+          });
+        break;//hexagon octagon
+      case "heptagon":
+        this.shapeSvc.getComplexShape(formData.shape, formData.side)
+          .subscribe(response => {
+            console.log([response.side, response.radius]);
+            this.drawPolygon(this.canvasContext, response.side, response.radius, response.x, response.y);
+
+          });
+        break;
+      case "octagon":
+        this.shapeSvc.getComplexShape(formData.shape, formData.side)
+          .subscribe(response => {
+            console.log([response.side, response.radius]);
+            this.drawPolygon(this.canvasContext, response.side, response.radius, response.x, response.y);
+
+          });
+        break;
+      case "square":
+        this.shapeSvc.getSquare(formData.shape, formData.side)
+          .subscribe(response => {
+            console.log([response.side, response.radius]);
+            this.drawSquare(this.canvasContext, response.measurement1, response.x, response.y);
+
+          });
+        break;
+      case "rectangle":
+        this.shapeSvc.getRectangle(formData.shape, formData.width, formData.height)
+          .subscribe(response => {
+            console.log([response.side, response.radius]);
+            this.drawRectangle(this.canvasContext, formData.width, formData.height, response.x, response.y);
+
+          });
+        break;
+      case "isosceles_triangle":
+        this.shapeSvc.getTriangle(formData.shape, formData.width, formData.height, canvasWidth, canvasHeight)
+          .subscribe(response => {
+            console.log([response.side, response.side2]);
+            this.drawisoscelesTriangle(this.canvasContext, formData.width, response.side2, response.x, response.y);
+
+          });
+        break;
+      case "equilateral_triangle":
+        this.shapeSvc.getTriangle(formData.shape, formData.width, formData.height, canvasWidth, canvasHeight)
+          .subscribe(response => {
+            console.log([response.side, response.side2, response.side3]);
+            this.drawEquilateralTriangle(this.canvasContext, response.side, response.side2, response.side3, response.x, response.y);
+
+          });
+        break;
+      case "parallelogram":
+        this.shapeSvc.getParallelogram(formData.shape, formData.width, formData.height)
+          .subscribe(response => {
+            console.log([canvasWidth, canvasHeight]);
+            this.drawParallelogram(this.canvasContext, response.pointA, response.pointB, response.pointC, response.pointD, response.pointE, response.pointF, response.pointG, response.pointH, formData.width, formData.height);
+
+          });
+        break;
+    }
 
 
 
@@ -72,24 +161,120 @@ export class ShapeCreateComponent implements OnInit {
     //   // this.drawTriangle(canvasContext, point1, point2, point3, true);    
   }
 
-  drawTriangle(context: any, side1: any, side2: any, side3: any, filled: boolean) {
+
+
+
+  // drawTriangle(context: any, side1: any, side2: any, side3: any, filled: boolean) {
+  //   context.beginPath();
+  //   context.moveTo(side1.x, side1.y);
+  //   context.lineTo(side2.x, side2.y);
+  //   context.lineTo(side3.x, side3.y);
+  //   context.closePath();
+  //   filled ? context.fill() : context.stroke();
+  // }
+
+  drawisoscelesTriangle(context: any, side: any, side2: any, x: any, y: any) {
+    context.reset();
+    context.translate(x, y);
     context.beginPath();
-    context.moveTo(side1.x, side1.y);
-    context.lineTo(side2.x, side2.y);
-    context.lineTo(side3.x, side3.y);
+    context.moveTo(0, -side);
+    context.lineTo(-side2, side);
+    context.lineTo(side2, side);
+    context.lineTo(0, -side);
     context.closePath();
-    filled ? context.fill() : context.stroke();
+    context.lineWidth = 2;
+    context.strokeStyle = '#118ab2';
+    context.stroke();
+  }
+
+  drawEquilateralTriangle(context: any, side: any, side2: any, side3: any, x: any, y: any) {
+    context.reset();
+    //context.translate(x, y);
+    context.beginPath();
+    context.moveTo(side, side);
+    context.lineTo(side, side2);
+    context.lineTo(side2, side2);
+    context.closePath();
+    context.lineWidth = 2;
+    context.strokeStyle = '#118ab2';
+    context.stroke();
   }
 
   drawCircle(context: any, canvas: any, radius: any, x: any, y: any) {
     context.reset();
 
-    // const x = canvas.nativeElement.width / 2;
-    // const y = canvas.nativeElement.height / 2;
-
     context.beginPath();
 
     context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.lineWidth = 2;
+    context.strokeStyle = '#118ab2';
+    context.stroke();
+  }
+
+  drawOval(context: any, canvas: any, radius1: any, radius2: any, x: any, y: any) {
+    context.reset();
+    context.beginPath();
+    context.ellipse(x, y, radius1, radius2, Math.PI / 2, 0, 2 * Math.PI);
+    context.stroke();
+  }
+
+
+  drawSquare(context: any, sides: any, x: any, y: any) {
+    context.reset();
+
+    context.beginPath();
+
+    context.rect(x, y, sides, sides)
+    context.lineWidth = 2;
+    context.strokeStyle = '#118ab2';
+    context.stroke();
+  }
+
+  drawRectangle(context: any, width: any, heigth: any, x: any, y: any) {
+    context.reset();
+
+    context.beginPath();
+
+    context.rect(x, y, width, heigth)
+    context.lineWidth = 2;
+    context.strokeStyle = '#118ab2';
+    context.stroke();
+  }
+
+  drawPolygon(context: any, sides: any, radius: any, x: any, y: any) {
+    context.reset();
+    context.translate(x, y);
+
+    for (let i = 0; i < sides; i++) {
+
+      const rotation = ((Math.PI * 2) / sides) * i;
+
+      if (i === 0) {
+        context.moveTo(radius * Math.cos(rotation), radius * Math.sin(rotation));
+      }
+      else {
+        context.lineTo(radius * Math.cos(rotation), radius * Math.sin(rotation));
+      }
+    }
+    context.closePath();
+    context.lineWidth = 2;
+    context.strokeStyle = '#118ab2';
+    context.stroke();
+
+  }
+
+  drawParallelogram(context: any, pointA: any, pointB: any, pointC: any, pointD: any, pointE: any, pointF: any, pointG: any, pointH: any, width: any, heigth: any) {
+    context.reset();
+
+    context.translate(width / 2, heigth / 2);
+    context.beginPath();
+
+    context.moveTo(pointA, pointB);
+    context.lineTo(pointC, pointD);
+    context.lineTo(pointE, pointF);
+    context.lineTo(pointG, pointH);
+    context.lineTo(pointA, pointB);
+    context.closePath();
     context.lineWidth = 2;
     context.strokeStyle = '#118ab2';
     context.stroke();
--- src/app/services/shape.service.ts
+++ src/app/services/shape.service.ts
@@ -8,14 +8,65 @@ export class ShapeService {
 
     constructor(private http: HttpClient) { }
 
-    private readonly APIUrl = "https://localhost:7277/GabrielAppTest/Shape?";
+    private readonly APIUrl = "https://localhost:7277/GabrielAppTest/";
 
 
-    getShape(radius: any, canvasWidth: any, canvasHeight: any) {
-        let ciculeAPI = `shapeType=circule&measurement1=${radius}&measurement2=0&canvasWidth=${canvasWidth}&canvasHeigth=${canvasHeight}`;
-        return this.http.get<any>(this.APIUrl + ciculeAPI);
+    getCicle(shape: any, radius: any, canvasWidth: any, canvasHeight: any) {
+        let circleAPI = `Shape?shapeType=${shape}&measurement1=${radius}&measurement2=0&canvasWidth=${canvasWidth}&canvasHeigth=${canvasHeight}`;
+        return this.http.get<any>(this.APIUrl + circleAPI);
     }
-    // getUsers(): Observable<any> {
-    //     return this.http.get<any>(this.url);
-    // }
+
+    getOval(shape: any, radius1: any, radius2: any, canvasWidth: any, canvasHeight: any) {
+        let ovalAPI = `Shape?shapeType=${shape}&measurement1=${radius1}&measurement2=${radius2}&canvasWidth=${canvasWidth}&canvasHeigth=${canvasHeight}`;
+        return this.http.get<any>(this.APIUrl + ovalAPI);
+    }
+
+    getSquare(shape: any, legnth: any) {
+        let rectangleAPI = `Shape?shapeType=${shape}&measurement1=${legnth}&measurement2=0`;
+        return this.http.get<any>(this.APIUrl + rectangleAPI);
+    }
+
+    getRectangle(shape: any, width: any, height: any) {
+        let rectangleAPI = `Shape?shapeType=${shape}&measurement1=${width}&measurement2=${height}`;
+        return this.http.get<any>(this.APIUrl + rectangleAPI);
+    }
+
+    getParallelogram(shape: any, width: any, height: any) {
+        let parallelogramAPI = `Parallelogram?shapeType=${shape}&measurement1=${width}&measurement2=${height}`;
+        return this.http.get<any>(this.APIUrl + parallelogramAPI);
+    }
+
+    getTriangle(shape: any, width: any, height: any, canvasWidth: any, canvasHeight: any) {
+        if (shape === "scalane_triangle") {
+            let triangleAPI = `Triangle?shapeType=${shape}&measurement1=${width}&measurement2=${height}&canvasWidth=${canvasWidth}&canvasHeigth=${canvasHeight}`;
+            return this.http.get<any>(this.APIUrl + triangleAPI);
+        }
+        if (shape === "isosceles_triangle") {
+            let triangleAPI = `Triangle?shapeType=${shape}&measurement1=${width}&measurement2=0&canvasWidth=${canvasWidth}&canvasHeigth=${canvasHeight}`;
+            return this.http.get<any>(this.APIUrl + triangleAPI);
+        }
+        let triangleAPI = `Triangle?shapeType=${shape}&measurement1=${width}&measurement2=${height ? height : 0}&canvasWidth=${canvasWidth}&canvasHeigth=${canvasHeight}`;
+        return this.http.get<any>(this.APIUrl + triangleAPI);
+    }
+
+    getComplexShape(shape: any, legnth: any) {
+
+        if (shape === 'pentagon') {
+            let complexShapeAPI = `ComplexShape?shapeNumSide=5&measurement1=${legnth}&measurement2=0`;
+            return this.http.get<any>(this.APIUrl + complexShapeAPI);
+        }
+        if (shape === 'hexagon') {
+            let complexShapeAPI = `ComplexShape?shapeNumSide=6&measurement1=${legnth}&measurement2=0`;
+            return this.http.get<any>(this.APIUrl + complexShapeAPI);
+        }
+        if (shape === 'heptagon') {
+            let complexShapeAPI = `ComplexShape?shapeNumSide=7&measurement1=${legnth}&measurement2=0`;
+            return this.http.get<any>(this.APIUrl + complexShapeAPI);
+        }
+
+        let complexShapeAPI = `ComplexShape?shapeNumSide=8&measurement1=${legnth}&measurement2=0`;
+        return this.http.get<any>(this.APIUrl + complexShapeAPI);
+
+    }
+
 }
